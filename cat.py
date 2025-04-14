087a228d765949c07ed30024acf535be11042e5b
Shivani <shivaniuniverse@gmail.com>
2025-04-14 22:58:01 +0530

    Create cat.py

---
+++ cat.py
@@ -0,0 +1,17 @@
+class Cat():
+    
+    species = "Abyssinian" #my cat
+    
+    def __init__(self, name, age):
+        
+        self.name = name
+        self.age = age
+        
+lilly = Cat("Lilly", 3)
+sona = Cat("Sona", 7)
+        
+print(lilly.age)
+print(sona.name)
+
+#3
+#Sona
